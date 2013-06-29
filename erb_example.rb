f160b911cd4c73253e83c5d6a781effb128b3866
David Broadlick <dbroadlick@gmail.com>
2013-06-29 14:15:36 -0500

    ex1

--- erb_example.rb
+++ erb_example.rb
@@ -1,30 +1,38 @@
 require 'erb'
-puts "How many slices of time would you like to record?"
-gets slices
+require 'time'
+
 =begin
 this method will take in slices, number of times to record the current time and a block which will be what to do with the time.
 =end
-def time (slices) 
-	all_time
+def time
+	alltime = Array.new
+	slices = 10
 	i = 0
-	while i < slices
-		all_time.push(Time.now)
-		i++
+	while i < slices.to_i do
+		alltime.push(Time.now)
+		i+=1
 	end
 	yield
 end
-
+=begin
 def print_time
 	i = 0
-	while i < all_time.length
+	while i < all_time.length do
 		puts all_time[i]
+		i+=1
 	end
 end
+=end
+puts "How many slices of time would you like to record?"
+slices = gets
 
-time(slices)
+#bockprint = { alltime.each do |e| print e end }
+
+time { alltime.each do |e| print e end }
 
 
-
+=begin
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+template = ERB.new ("The value of x is: <%= x %>")
 puts template.result(binding)
+=end
